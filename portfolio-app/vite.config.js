729a33a8b7893440bc4657bff7b99fd7b87ef0d9
konsoul <brad.rappa@gmail.com>
2024-12-28 22:37:28 -0800

    adjuste base

--- portfolio-app/vite.config.js
+++ portfolio-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/konsoul.github.io/',
+  base: '/konsoul.github.io/portfolio-app',
 })
